11d93057e0db6a08ce59af2f71d1147821d06a71
NethanosGit <razudesu31@gmail.com>
2023-10-12 11:19:19 +0300

    ssh

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   publicPath:
-    process.env.NODE_ENV === "production" ? "/dnd5e-spellsbook-deploy/" : "/",
+    process.env.NODE_ENV === "production" ? "/dnd5e-spellsbook/" : "/",
   transpileDependencies: true,
 });
