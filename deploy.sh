39e8ef9ea10d74d3945dc1c5e81e429dfe589e84
NethanosGit <razudesu31@gmail.com>
2023-10-12 11:03:54 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:NethanosGit/dnd5e-spellsbook-deploy.git master:gh-pages
+git push -f git@github.com:NethanosGit/dnd5e-spellsbook.git master:gh-pages
 
 cd -
