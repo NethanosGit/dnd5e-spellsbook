c29d6ff1270fdd04dd69c100ce9de2f18fd094c4
NethanosGit <razudesu31@gmail.com>
2023-10-12 15:17:57 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -124,7 +124,7 @@ export default {
       this.awaitingInitialFetch = true;
       Promise.all(
         defaultContentFileURLs.map((url) =>
-          this.app.contentDatabase.loadURL(`${url}`)
+          this.app.contentDatabase.loadURL(url)
         )
       ).then(() => {
         this.app.reloadDatabase();
