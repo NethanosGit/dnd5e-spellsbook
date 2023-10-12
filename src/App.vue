1ad87f1f25842c319b3789bf1dd0e6644aff7ad8
NethanosGit <razudesu31@gmail.com>
2023-10-12 14:42:55 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -124,8 +124,11 @@ export default {
       this.awaitingInitialFetch = true;
       Promise.all(
         defaultContentFileURLs.map((url) => {
-          url = process.env.NODE_ENV === "production" ? `${url}` : url;
-          this.app.contentDatabase.loadURL(url);
+          this.app.contentDatabase.loadURL(
+            process.env.NODE_ENV === "production"
+              ? `dnd5e-spellsbook${url}`
+              : url
+          );
         })
       ).then(() => {
         this.app.reloadDatabase();
